2486309615d732f20a88c79a4f832e0a1b724204
arjunvnd <arjunvnd@gmail.com>
2020-07-18 18:58:34 +0530

    Updates

---
+++ precache-manifest.0dc49aa4436caa89f2a194f5e9cd7496.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d9e159d71d2488d0e6ab04c0e94408",
+    "url": "/watch-eat/index.html"
+  },
+  {
+    "revision": "f0c92eaf85703eb55b93",
+    "url": "/watch-eat/static/css/main.82e0a6e5.chunk.css"
+  },
+  {
+    "revision": "6811aa9985ff56fc348c",
+    "url": "/watch-eat/static/js/2.dd190d02.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/watch-eat/static/js/2.dd190d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0c92eaf85703eb55b93",
+    "url": "/watch-eat/static/js/main.7ce04a27.chunk.js"
+  },
+  {
+    "revision": "cba0d7bd7107fcf89eba",
+    "url": "/watch-eat/static/js/runtime-main.ec383a5d.js"
+  }
+]);
